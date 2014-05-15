b386c8e90389935e40b79845b65d983a6557c5e4
Andrew Cox <andrew@andrewcox.org>
2014-05-15 11:57:14 -0400

    Set up skeleton elevator kata

--- elevator-kata-tests.js
+++ elevator-kata-tests.js
@@ -1,3 +1,27 @@
+var log = function (msg) {
+  console.log(msg);
+};
+
+// Example input
+var calls = [
+  {
+    origin: 1,
+    destination: 10
+  },
+  {
+    origin: 5,
+    destination: 10
+  },
+  {
+    origin: 8,
+    destination: 2
+  },
+  {
+    origin: 1,
+    destination: 6
+  }
+];
+
 test('hello world', function () {
 
   // ok( truthy[, message ] )
@@ -13,4 +37,20 @@ test('an elevator', function () {
   var elevator = new Elevator(5);
 
   equal(elevator.floors, 5, "has a number of floors");
+  equal(calls.length, 4, "there are 4 calls");
+});
+
+test('simulation', function () {
+  var elevator = new Elevator(10);
+  var $building = $("#building");
+
+  for (var ii = 0; ii < elevator.floors; ii++) {
+    $building.append(
+      "<div class='floor floor" + (ii + 1) + "'>" +
+        "<div class='waiting'></div>" +
+        "<div class='elevator'></div>" +
+        "<div class='arrived'></div>" +
+      "</div>"
+    );
+  }
 });
